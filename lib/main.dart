95ede9161298dd7d5919b3dcf2d66b613dcc3a9c
minasalah555 <salahmina452@gmail.com>
2025-04-02 19:51:20 +0200

    privide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/Add_notes_cubit/add_notes__cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/notes_view.dart';
@@ -17,18 +19,21 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        NotesView.id: (context) => NotesView(),
-        EditNoteView.id: (context) => EditNoteView(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
-        // scaffoldBackgroundColor: Color.fromARGB(255, 80, 152, 228)
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (Context) => AddNotesCubit())],
+      child: MaterialApp(
+        routes: {
+          NotesView.id: (context) => NotesView(),
+          EditNoteView.id: (context) => EditNoteView(),
+        },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          // scaffoldBackgroundColor: Color.fromARGB(255, 80, 152, 228)
+        ),
+        initialRoute: NotesView.id,
       ),
-      initialRoute: NotesView.id,
     );
   }
 }
